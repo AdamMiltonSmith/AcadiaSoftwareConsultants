18d57ec3a6f08cb185d713bd71bc33bdee125553
jlegere45 <jlegere45@gmail.com>
2019-12-03 15:19:41 -0500

    Create data_controller.py
    The is the file that will be used to instantiate and manipulate data objects

---
+++ icesat2/data/data_controller.py
@@ -0,0 +1,35 @@
+import os
+import datetime
+
+class data:
+    day_delta = datetime.timedelta(days=1)
+    name = "Untitled"
+
+    def __init__(self, start_date, end_date, minx, miny, maxx, maxy, name=None, day_delta=None):
+        self.start_date = start_date
+        self.end_date = end_date
+
+        self.minx = minx
+        self.miny = miny
+        self.maxx = maxx
+        self.maxy = maxy
+
+        if(day_delta != None):
+            self.day_delta = day_delta
+
+        if(name != None):
+            self.name = name
+
+    def get_data(self):
+        for i in range((end_date - start_date).days):
+            current = "https://openaltimetry.org/data/api/icesat2/atl06?date="+ i +"&minx="+ minx +"&miny="+ miny +"&maxx="+ maxx +"&maxy="+ maxy +"&trackId=705&client=jupyter&outputFormat=csv"
+            os.system('curl -X GET' + current + '-H' '"accept: */*" >>' + name)
+    
+    #This function will return a csv file of the difference in height from the start date to the end date
+    def get_height_diff(self):
+        return
+    
+    #This function will return a csv file the height differentials, the differential will be calculate useing all the dates
+    #This will be a bit harder to implement 
+    def get_differential(self):
+        return
