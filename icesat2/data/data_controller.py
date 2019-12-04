f0fb2c09e70c65e84efbae93307c5d77acdcda81
jlegere45 <jlegere45@gmail.com>
2019-12-03 20:24:41 -0500

    Update data_controller.py

--- icesat2/data/data_controller.py
+++ icesat2/data/data_controller.py
@@ -1,11 +1,14 @@
 import os
 import datetime
+from datetime import date
 
+#csv format:
+#   segment_id, longitude, latitude, height, quality, track_id, beam, file_name
 class data:
     day_delta = datetime.timedelta(days=1)
-    name = "Untitled"
+    file_name = "Untitled"
 
-    def __init__(self, start_date, end_date, minx, miny, maxx, maxy, name=None, day_delta=None):
+    def __init__(self, start_date, end_date, minx, miny, maxx, maxy, file_name=None, day_delta=None):
         self.start_date = start_date
         self.end_date = end_date
 
@@ -17,19 +20,34 @@ class data:
         if(day_delta != None):
             self.day_delta = day_delta
 
-        if(name != None):
-            self.name = name
+        if(file_name != None):
+            self.file_name = file_name
 
     def get_data(self):
-        for i in range((end_date - start_date).days):
-            current = "https://openaltimetry.org/data/api/icesat2/atl06?date="+ i +"&minx="+ minx +"&miny="+ miny +"&maxx="+ maxx +"&maxy="+ maxy +"&trackId=705&client=jupyter&outputFormat=csv"
-            os.system('curl -X GET' + current + '-H' '"accept: */*" >>' + name)
+        for i in range((self.end_date - self.start_date).days):
+            day = (self.start_date + i*self.day_delta)
+            current = '"https://openaltimetry.org/data/api/icesat2/atl06?date='+ day.strftime('%Y-%m-%d') +'&minx='+ self.minx +'&miny='+ self.miny +'&maxx='+ self.maxx +'&maxy='+ self.maxy +'&trackId=705&client=jupyter&outputFormat=csv"'
+            command = 'curl -X GET ' + current + ' -H' + " accept: */*"
+            print (command)
+            os.system(command + " >> " + self.file_name)
+            os.system("echo " + day.strftime('%Y-%m-%d') + " >> " + self.file_name + "_dates")
+        return
     
     #This function will return a csv file of the difference in height from the start date to the end date
-    def get_height_diff(self):
-        return
+
+    #make dict with key being lat, each key will point to a height and beam number
+    #upon finding the same late again,
+    #iterate through
+    # def get_height_diff(self):
+    #     with open(self.file_name) as f:
+            
+    #     return
     
     #This function will return a csv file the height differentials, the differential will be calculate useing all the dates
     #This will be a bit harder to implement 
     def get_differential(self):
         return
+
+#test command input
+x = data(date(2018, 11, 13), date(2018, 11, 15), '105.25', '49.48', '106.06', '50.43', "test")
+x.get_data()
