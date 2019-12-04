310753035e84a8934b66c902e7e32ebf50b40e0b
Jacob R Boudreau <jacob.boudreau8@gmail.com>
2019-12-04 13:19:28 -0500

    Added comments

--- graphTests/temp.py
+++ graphTests/temp.py
@@ -4,6 +4,7 @@ from kivy.uix.boxlayout import BoxLayout
 import matplotlib.pyplot as plt
 import csv 
 
+#Reads from CSV file and adds them to a list csvInput
 with open('foo.txt') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter= ',')
     csvInput = []
@@ -11,16 +12,14 @@ with open('foo.txt') as csv_file:
         for element in row:
             csvInput.append(int(element))
         
-        
-print(csvInput)      
-
+#Plots the elements read from csvInput
 plt.plot(csvInput)
 plt.title('IGLOO TESTS')
 plt.ylabel('Y Axis')
 plt.xlabel('X Axis')
 
+#Adds the app to a Kivy app
 class MyApp(App):
-
     def build(self):
         box = BoxLayout()
         box.add_widget(FigureCanvasKivyAgg(plt.gcf()))
