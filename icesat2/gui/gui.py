bff61e314bb7c91d0b67b4cfe0f2fb2b26833328
Jacob R Boudreau <jacob.boudreau8@gmail.com>
2020-02-18 14:57:29 -0500

    Fixed some conflicts with master.

--- icesat2/gui/gui.py
+++ icesat2/gui/gui.py
@@ -7,53 +7,50 @@ from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.splitter import Splitter
 from kivy.core.window import Window
 import kivy.properties as prop
-from os import listdir
-from os.path import isfile, join
+from kivy.config import Config
+from math import sin
+from kivy.garden.graph import Graph, MeshLinePlot
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty  
+from kivy.clock import Clock
+from os import listdir
+from os.path import isfile, join
 import glob
 import os
 import icesat2.graph.graphPngExport as graphPngExport
-from kivy.garden.graph import Graph, MeshLinePlot
 
 
-# def pre_init_screen():
-#     import sys
 
+def pre_init_screen():
+    import tkinter as tk
 
-#     """
-#     returns Monitor size x and y in pixels for desktop platforms, or None for
-#     mobile platforms
-#     Found at:
-#     https://groups.google.com/forum/#!topic/kivy-users/uZYrghb87g0
-#     """
-#     if sys.platform == 'linux2':
-#         import subprocess
-#         output = subprocess.Popen(
-#             'xrandr | grep "\*" | cut -d" " -f4',
-#             shell=True,
-#             stdout=subprocess.PIPE).communicate()[0]
-#         screenx = int(output.replace('\n', '').split('x')[0])
-#         screeny = int(output.replace('\n', '').split('x')[1])
-#     elif sys.platform == 'win32':
-#         from pywin32 import GetSystemMetrics
-#         screenx = GetSystemMetrics(0)
-#         screeny = GetSystemMetrics(1)
-#     elif sys.platform == 'darwin':
-#         from AppKit import NSScreen
-#         frame_size = NSScreen.mainScreen().frame().size
-#         return frame_size.width, frame_size.height
-#     else:
-#         # For mobile devices, use full screen
-#         screenx, screeny = 800, 600  # return something
+    screen = tk.Tk()
+
+    screenx, screeny = screen.winfo_screenwidth(), screen.winfo_screenheight()
 
 class MainApp(App):
+   
+    assigned = False
+    carrot = ObjectProperty(None)
     def build(self):
         self.title = "IGLOO"
         b = Builder.load_file("icesat2\\gui\\kv\\gui.kv")
-        #Window.size = (1920, 1080)
-        return b
 
+
+        #Clock.schedule_interval(self.update, 1)
+        return b
+    
+    # def update(self, *args):
+    #     print("test")
+        
+    # def assign(self, booten):
+    #     if assigned == False:
+    #         print("<Assigning Shit")
+    #         carrot = booten
+    #         assigned = True
+    #         carrot.doTheThing()
+        
+        
 
 
 class TopButton(Button):
@@ -88,43 +85,54 @@ class DefaultButton(Button):
     #btn_width = prop.NumericProperty(70)
     side_width_buffer = prop.NumericProperty(20)
 
+class DataSetRefreshButton(Button):
+    font_size = prop.NumericProperty(14)
+    back_color = prop.ColorProperty([0.5, 0.5, 0.5, 1.0])
+
+    text_color = prop.ColorProperty([1.0, 1.0, 1.0, 1.0])
+    btn_height = prop.NumericProperty(20)
+    #btn_width = prop.NumericProperty(70)
+    side_width_buffer = prop.NumericProperty(20)
 
     container = ObjectProperty(None) #container the buttons are added to
     def add_buttons(self):
-        datasetPath = "resources\\csv_data_collection"
-        files = listdir(datasetPath)
+        datasetPath = "resources"
+        #files = listdir(datasetPath)
+        files = next(os.walk(datasetPath))[1]
         print(files)
         for f in files:
-            self.container.add_widget(Button(text=f, id=str(f)))
-
-    # Currently, the refresh button gets replaced by a duplicate of the last button
-    # after the first refresh
+            tempButton = DefaultButton()
+            tempButton.text = f
+            self.container.add_widget(tempButton)
+    #remove buttons
     def remove_buttons(self):
-        skipfirst = 0 #prevents the refresh button from being removed
         for child in [child for child in self.container.children]:
-            if skipfirst != 0:
                 self.container.remove_widget(child)
-            else:
-                skipfirst += 1
+    def doTheThing(self):
+        print("Doing the thing")
 
 
 class Main_Window(Screen):
     def __init__(self, **kw):
         super(Main_Window, self).__init__(**kw)
 
-        
 class Graph_Window(Screen):
     def __init__(self, **kw):
         super(Graph_Window, self).__init__(**kw)
 
+class Map_Window(Screen):
+    def __init__(self, **kw):
+        super(Map_Window, self).__init__(**kw)
 
 class ScreenManagement(ScreenManager):
-    #pre_init_screen()
+    pre_init_screen()
     pass
 
 class WindowSplitter(Splitter):
     border_size = prop.NumericProperty(5)
 
+class SetGraph(Widget):
+    testGraph = ObjectProperty(None)
 
 # sm = ScreenManager()
 
