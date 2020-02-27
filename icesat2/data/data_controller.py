da2f0a7d550b498c88ae7023e2edcd0b8f61b7b7
jlegere45 <jlegere45@gmail.com>
2020-02-26 19:37:18 -0500

    fixes

--- icesat2/data/data_controller.py
+++ icesat2/data/data_controller.py
@@ -2,6 +2,9 @@ import os
 import datetime
 import re
 import pandas as pd
+import numpy as np
+import requests
+
 from os import listdir
 from os.path import isfile, join
 from datetime import date
@@ -9,17 +12,28 @@ from datetime import date
 #csv format:
 #   segment_id, longitude, latitude, height, quality, track_id, beam, file_name
 
+min_x = '105.25'
+min_y = '49.48'
+max_x = '106.06'
+max_y = '50.43'
+
 path = "resources\\csv_data_collection"
 class Data:
     day_delta = datetime.timedelta(days=1)
     file_name = "Untitled"
 
-    def __init__(self, start_date=None, end_date=None, file_name=None, day_delta=None):
+    def __init__(self, start_date=None, end_date=None, min_x=None, min_y=None, max_x=None, max_y=None, file_name=None, day_delta=None):
         self.start_date = start_date
         self.end_date = end_date
 
         self.avg_height = 0
         self.stdv = 0
+
+        self.min_x = min_x
+        self.min_y = min_y
+
+        self.max_x = max_x
+        self.max_y = max_y
 
         #this counts the number of csv files in the directory
         self.file_count = 0
@@ -35,17 +49,26 @@ class Data:
         if not os.path.exists(self.path):
             os.makedirs(self.path)
 
-    def get_data(self, minx, miny, maxx, maxy):
-        #offset = 7
+    def get_data(self):
+        """
+        This function makes GET request to the api provided by openaltimetry
+        and saves the results in a csv format
+        """
         for i in range((self.end_date - self.start_date).days + 1):
             day = (self.start_date + i*self.day_delta)
-            current = '"https://openaltimetry.org/data/api/icesat2/atl06?date='+ day.strftime('%Y-%m-%d') +'&minx='+ minx +'&miny='+ miny +'&maxx='+ maxx +'&maxy='+ maxy +'&trackId=705&client=jupyter&outputFormat=csv"'
-            command = 'curl -X GET ' + current + ' -H' + " accept: */*"
+            
+            parameters = {'date': day.strftime('%Y-%m-%d'), 'minx': self.min_x, 'miny': self.min_y, 'maxx' : self.max_x, 'maxy': self.max_y, 'trackId': '705', 'client': 'jupyter', 'outputFormat': 'csv'}           
+            url = "https://openaltimetry.org/data/api/icesat2/atl06"
+            
+            r = requests.get(url, params=parameters)
+            write_file = self.path + "/" + self.file_name + day.strftime('%Y-%m-%d')
+            
+            with open(write_file, "w") as f:
+                for line in r.text:
+                    f.write(line)
 
-            os.system(command + " >> " + self.path + self.file_name)
-        
         #Get the dates
-        os.system(self.start_date + " " + self.end_date + " >> dates")
+        #os.system(self.start_date + " " + self.end_date + " >> " + self.path + self.file_name + "_info")
     
     """
     get_height, will subtract height at lat x at time b from height at lat x from time a
@@ -113,5 +136,6 @@ def fetchData(file_name):
     data.get_dates()
 
     return data
-x = Data(date(2018, 11, 13), date(2018, 11, 15), "test")
-test = pd.read_csv("resources/csv_data_collection/test", index_col=None)
+
+x = Data(date(2018, 11, 13), date(2018, 11, 15), min_x, min_y, max_x, max_y, "test")
+x.get_data()
