4faae9d05cb4ec2af00f620a0e867e8116e223d7
JoshuaSchaff <schaffjosh@gmail.com>
2020-04-20 13:48:04 -0400

    Updated Map

--- icesat2/gui/CanvasMessingAround.py
+++ icesat2/gui/CanvasMessingAround.py
@@ -5,9 +5,11 @@ from kivy.uix.button import Button
 from kivy.graphics import Rectangle
 from kivy.graphics import Color
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.clock import Clock
 from kivy.uix.image import Image
 from kivy.uix.textinput import TextInput
+from kivy.uix.stencilview import StencilView
 import ctypes
 
 
@@ -21,63 +23,132 @@ libbytiff.TIFFSetWarningHandler.argtypes = [ctypes.c_void_p]
 libbytiff.TIFFSetWarningHandler.restype = ctypes.c_void_p
 libbytiff.TIFFSetWarningHandler(None)
 
-class ContainerBox(BoxLayout):
-    def __init__(self, **kwargs):
-        super(ContainerBox, self).__init__(**kwargs)
-        self.orientation = 'vertical'
-        #self.picture = Image(allow_stretch=True, source='..\pics\lugia.png')
-        self.picture = Image(allow_stretch=False, source='resources\\map_images\\ice_field_map.TIF')
-        Clock.schedule_once(lambda dt: self.add_widget(self.picture), timeout=0.1)
+
+class StencilTestWidget(StencilView):
+    '''Drag to define stencil area
+    '''
+
+    def doNothing(self):
+        print("doin nothin")
+
 
 
 class Touch(BoxLayout):
-
-   
-
     def __init__(self, **kwargs):
         super(Touch, self).__init__(**kwargs)
-        
-        
         self.selectBox = True
+        
         self.orientation = 'vertical'
-        self.picture = Image(allow_stretch=True, source='resources\\map_images\\ice_field_map2.TIF')
-        #Clock.schedule_once(lambda dt: self.add_widget(self.picture), timeout=0.1)
-        self.add_widget(self.picture)
-       
+        
+        self.mainBox = BoxLayout(orientation = 'horizontal')
+        self.add_widget(self.mainBox)
+        
 
+
+        self.picture = Image(allow_stretch=True, source='resources\\map_images\\ice_field_map3.TIF')
+        self.mainBox.add_widget(self.picture)
+        # xmin,xmax,ymin,xmax of the provided image
+        self.xminMap = 134.601389
+        self.xmaxMap = 134.956389
+        self.yminMap = 58.363611
+        self.ymaxMap = 58.989167
+        
+        self.xdiff = self.xmaxMap - self.xminMap
+        self.ydiff = self.ymaxMap - self.yminMap
+        
   
         #draw a rectangle
         with self.canvas:
             Color(1,0,0,.5,mode='rgba')
-            self.rect = Rectangle(pos=(0,0), size=(0,0))
+            absImageX = self.picture.center_x - self.picture.norm_image_size[0] / 2.0
+            absImageY = self.picture.center_y + self.picture.norm_image_size[1] / 2.0
+
+
+            self.rect = Rectangle(pos=(absImageX, absImageY), size=(50,50))
+        
+        
+        self.buttonBox = BoxLayout(orientation = 'vertical')
+        self.mainBox.add_widget(self.buttonBox)
+        btnCurrentBox = Button(text = 'Box to Current Data Set')
+        btnPullData = Button(text = 'Pull Box Data')
+        self.buttonBox.add_widget(btnCurrentBox)
+        self.buttonBox.add_widget(btnPullData)
+        
         
 
-        self.bottomBox = BoxLayout(orientation = 'horizontal')
-        self.bottomBox.height = 100
-        toggleButton = Button(text = 'Toggle Bounding Box Select')
-        textinput = TextInput(text='Hi')
-        self.bottomBox.add_widget(textinput)
-        self.bottomBox.add_widget(toggleButton)
- 
-        self.add_widget(self.bottomBox)
+    def getImageAbsX(self):
+        return self.picture.center_x - self.picture.norm_image_size[0] / 2.0   
 
+    def getImageAbsY(self):
+        return self.picture.center_y + self.picture.norm_image_size[1] / 2.0
 
     def on_touch_down(self, touch):
-        self.rect.pos = touch.pos
-        self.rect.size = (1, 1)
-        print("Mouse Down", touch)
+        if self.selectBox == True:
+            #check if selection is being done outside of the actual image not just the image widget
+            out_of_bounds = False
+            if (touch.x < self.picture.center_x - self.picture.norm_image_size[0] / 2) or (touch.x > self.picture.center_x - self.picture.norm_image_size[0] / 2.0 + self.picture.norm_image_size[0]):
+                out_of_bounds = True
+            if (touch.y < self.picture.center_y - self.picture.norm_image_size[1] / 2) or (touch.y > self.picture.center_y - self.picture.norm_image_size[1] / 2.0 + self.picture.norm_image_size[1]):
+                out_of_bounds = True
+            if out_of_bounds:
+                pass
+            else:
+                self.rect.pos = touch.pos
+                self.rect.size = (0, 0)
 
     def on_touch_move(self, touch):
-        self.rect.size = (touch.x - touch.ox, touch.y - touch.oy)
-        print("Mouse Move", touch)
+        if self.selectBox == True:
+            #check if selection is being done outside of the actual image not just the image widget
+            out_of_bounds = False
+            if (touch.x < self.picture.center_x - self.picture.norm_image_size[0] / 2) or (touch.x > self.picture.center_x - self.picture.norm_image_size[0] / 2.0 + self.picture.norm_image_size[0]):
+                out_of_bounds = True
+            if (touch.y < self.picture.center_y - self.picture.norm_image_size[1] / 2) or (touch.y > self.picture.center_y - self.picture.norm_image_size[1] / 2.0 + self.picture.norm_image_size[1]):
+                out_of_bounds = True
+            if out_of_bounds:
+                pass
+            else:
+                self.rect.size = (touch.x - touch.ox, touch.y - touch.oy)
+    
+    #set the box position and size based on xmin, xmax, ymin, ymax
+    def set_box_pos(self,xmin,xmax,ymin,ymax):
+        bottomX = 0
+        bottomY = 0
+        xsize = 0
+        ysize = 0
+
+        #Check if coords are within range of image
+        inBounds = True
+        if (xmin >= self.xminMap) and (xmax < self.xmaxMap) and (ymin >= self.yminMap) and (ymax < self.ymaxMap):
+            inBounds =  True
+        else:
+            inBounds = False
+        
+        if inBounds == True:
+            #convert actual coords of bottom left of box to relative pixels on map image
+            totalPixelsX = (self.picture.center_x - self.picture.norm_image_size[0] / 2.0 + self.picture.norm_image_size[0])
+            totalPixelsY = (self.picture.center_y - self.picture.norm_image_size[1] / 2.0 + self.picture.norm_image_size[1]) - (self.picture.center_y - self.picture.norm_image_size[1] / 2)
+            distanceRatioX = (self.xmaxMap - xmin) / (self.xmaxMap - self.xminMap)
+            distanceRatioY = (self.ymaxMap - ymin) / (self.ymaxMap - self.yminMap)
+            bottomX = totalPixelsX * distanceRatioX
+            bottomY = totalPixelsY * distanceRatioY
+
+            #convert xmax and ymax for size 
+            distanceRatioX = (self.xmaxMap - xmax) / (self.xmaxMap - self.xminMap)
+            distanceRatioY = (self.ymaxMap - ymax) / (self.ymaxMap - self.yminMap)
+            xsize = (totalPixelsX * distanceRatioX) - bottomX
+            ysize = (totalPixelsY * distanceRatioY) - bottomY
+
+        self.rect.pos = (bottomX,bottomY)
+        self.rect.size = (xsize, ysize)
+
+
+    def pullData(self):
+        print("Pulling data from selected region on map")
     
 
-    def set_box_pos(self,x,y,sizex,sizey):
-        self.rect.pos = (x,y)
-        self.rect.size = (sizex, sizey)
 
-    def _on_keyboard_down(self):
-        print("keydown")
+    
+
 
 
 
--- icesat2/gui/gui.py
+++ icesat2/gui/gui.py
@@ -431,9 +431,6 @@ def is_float(input):
     except:
         return False
 
-class ObamaButton(BoxLayout):
-    font_size = prop.NumericProperty(30)
-
 class DataSetRefreshButton(Button):
     font_size = prop.NumericProperty(14)
     back_color = prop.ColorProperty([0.5, 0.5, 0.5, 1.0])
@@ -476,34 +473,121 @@ class Graph_Window(Screen):
 class Map_Window(Screen):
     def __init__(self, **kwargs):
         super(Map_Window, self).__init__(**kwargs)
+        self.selectBox = True
         
-    
-    
+        self.orientation = 'vertical'
+        
+        self.mainBox = BoxLayout(orientation = 'horizontal')
+        self.add_widget(self.mainBox)
+        
+
+
+        self.picture = Image(allow_stretch=True, source='resources\\map_images\\ice_field_map3.TIF')
+        self.mainBox.add_widget(self.picture)
+        # xmin,xmax,ymin,xmax of the provided image
+        self.xminMap = 134.601389
+        self.xmaxMap = 134.956389
+        self.yminMap = 58.363611
+        self.ymaxMap = 58.989167
+        
+        self.xdiff = self.xmaxMap - self.xminMap
+        self.ydiff = self.ymaxMap - self.yminMap
+        
+  
         #draw a rectangle
         with self.canvas:
             Color(1,0,0,.5,mode='rgba')
-            self.rect = Rectangle(pos=(0,0), size=(0,0))
+            absImageX = self.picture.center_x - self.picture.norm_image_size[0] / 2.0
+            absImageY = self.picture.center_y + self.picture.norm_image_size[1] / 2.0
+
+
+            self.rect = Rectangle(pos=(300, 400), size=(50,50))
+        
+        
+        self.buttonBox = BoxLayout(orientation = 'vertical')
+        #self.mainBox.add_widget(self.buttonBox)
+        btnCurrentBox = Button(text = 'Box to Current Data Set')
+        btnPullData = Button(text = 'Pull Box Data')
+        self.buttonBox.add_widget(btnCurrentBox)
+        self.buttonBox.add_widget(btnPullData)
+        
         
 
+    def getImageAbsX(self):
+        return self.picture.center_x - self.picture.norm_image_size[0] / 2.0   
 
+    def getImageAbsY(self):
+        return self.picture.center_y + self.picture.norm_image_size[1] / 2.0
 
     def on_touch_down(self, touch):
-        self.rect.pos = touch.pos
-        self.rect.size = (1, 1)
-        print("Mouse Down", touch)
+        if self.selectBox == True:
+            #check if selection is being done outside of the actual image not just the image widget
+            out_of_bounds = False
+            if (touch.x < self.picture.center_x - self.picture.norm_image_size[0] / 2) or (touch.x > self.picture.center_x - self.picture.norm_image_size[0] / 2.0 + self.picture.norm_image_size[0]):
+                out_of_bounds = True
+            if (touch.y < self.picture.center_y - self.picture.norm_image_size[1] / 2) or (touch.y > self.picture.center_y - self.picture.norm_image_size[1] / 2.0 + self.picture.norm_image_size[1]):
+                out_of_bounds = True
+            if out_of_bounds:
+                pass
+            else:
+                self.rect.pos = touch.pos
+                self.rect.size = (0, 0)
 
     def on_touch_move(self, touch):
-        self.rect.size = (touch.x - touch.ox, touch.y - touch.oy)
-        print("Mouse Move", touch)
+        if self.selectBox == True:
+            #check if selection is being done outside of the actual image not just the image widget
+            out_of_bounds = False
+            if (touch.x < self.picture.center_x - self.picture.norm_image_size[0] / 2) or (touch.x > self.picture.center_x - self.picture.norm_image_size[0] / 2.0 + self.picture.norm_image_size[0]):
+                out_of_bounds = True
+            if (touch.y < self.picture.center_y - self.picture.norm_image_size[1] / 2) or (touch.y > self.picture.center_y - self.picture.norm_image_size[1] / 2.0 + self.picture.norm_image_size[1]):
+                out_of_bounds = True
+            if out_of_bounds:
+                pass
+            else:
+                self.rect.size = (touch.x - touch.ox, touch.y - touch.oy)
     
+    #set the box position and size based on xmin, xmax, ymin, ymax
+    def set_box_pos(self,xmin,xmax,ymin,ymax):
+        bottomX = 0
+        bottomY = 0
+        xsize = 0
+        ysize = 0
 
-    def set_box_pos(self,x,y,sizex,sizey):
-        self.rect.pos = (x,y)
-        self.rect.size = (sizex, sizey)
+        #Check if coords are within range of image
+        inBounds = True
+        if (xmin >= self.xminMap) and (xmax < self.xmaxMap) and (ymin >= self.yminMap) and (ymax < self.ymaxMap):
+            inBounds =  True
+        else:
+            inBounds = False
+        
+        if inBounds == True:
+            #convert actual coords of bottom left of box to relative pixels on map image
+            totalPixelsX = (self.picture.center_x - self.picture.norm_image_size[0] / 2.0 + self.picture.norm_image_size[0])
+            totalPixelsY = (self.picture.center_y - self.picture.norm_image_size[1] / 2.0 + self.picture.norm_image_size[1]) - (self.picture.center_y - self.picture.norm_image_size[1] / 2)
+            distanceRatioX = (self.xmaxMap - xmin) / (self.xmaxMap - self.xminMap)
+            distanceRatioY = (self.ymaxMap - ymin) / (self.ymaxMap - self.yminMap)
+            bottomX = totalPixelsX * distanceRatioX
+            bottomY = totalPixelsY * distanceRatioY
 
-    def _on_keyboard_down(self):
-        print("keydown")
+            #convert xmax and ymax for size 
+            distanceRatioX = (self.xmaxMap - xmax) / (self.xmaxMap - self.xminMap)
+            distanceRatioY = (self.ymaxMap - ymax) / (self.ymaxMap - self.yminMap)
+            xsize = (totalPixelsX * distanceRatioX) - bottomX
+            ysize = (totalPixelsY * distanceRatioY) - bottomY
 
+        self.rect.pos = (bottomX,bottomY)
+        self.rect.size = (xsize, ysize)
+
+
+    def pullData(self):
+        print("Pulling data from selected region on map")
+
+
+    def process_input(self, coord, start_date, end_date):
+        from icesat2.data.data_controller import Data
+        coord = [min_x, max_x, min_y, max_y]
+        d = Data(start_date = start_date, end_date=end_date, min_x = coord[0],
+                 min_y = coord[2], max_x = coord[1], max_y = coord[3])
 
 
 
