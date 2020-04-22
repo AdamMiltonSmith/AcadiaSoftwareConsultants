4ea350f39401e787352364a14f0679b9b42eec57
Adam Green <31974406+ItsmeCurly@users.noreply.github.com>
2020-04-22 05:05:01 -0400

    Delete plottest.py

--- plottest.py
+++
@@ -1,7 +0,0 @@
-import plotly.express as px
-px.set_mapbox_access_token(
-    "pk.eyJ1IjoieHlsZWNpb24iLCJhIjoiY2s2azJtaGV2MDJnNTNra3kxYmZ3YTlzdiJ9.s_EicZR1hfYNpWxZJfLMSQ")
-df = px.data.carshare()
-fig = px.scatter_mapbox(df, lat="centroid_lat", lon="centroid_lon",     color="peak_hour", size="car_hours",
-                        color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10)
-fig.show()
